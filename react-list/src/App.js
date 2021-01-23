041ef49f83cc12cfac558b0de779804375f78321
AmalaMulyasari <amala_mulyasari_28rpl@student.smktelkom-mlg.sch.id>
2021-01-23 20:09:16 +0700

    react-js

---
+++ react-list/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Shopee from './pages/shopee'
+import HariBesar from './pages/HariBesar'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <p><HariBesar/></p>
+        <p><Shopee/> </p>
+
+      </div>
+    )
+  }
+}
+
+export default App;
